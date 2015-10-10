1c2bfaf3113aeb7670e7288da003282a607eec1f
Sunjay Varma <varma.sunjay@gmail.com>
2015-10-10 16:05:22 -0400

    Script for downloading everything as a zip

--- js/index.js
+++ js/index.js
@@ -3,52 +3,77 @@ var WINDOW_HEIGHT = 400;
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, WINDOW_WIDTH / WINDOW_HEIGHT, 0.1, 1000);
-camera.position.set(10, 50, -10);
+camera.position.set(4, 2, -4);
 camera.lookAt(scene.position);
 
-var light = new THREE.AmbientLight(0xFFFFFF);
+var light = new THREE.SpotLight(0xFFFFFF, 1, 100);
+light.position.set(10, 10, -10);
 scene.add(light);
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-renderer.setClearColor(0xEEEEEE, 1);
-document.getElementById("main-container").appendChild(renderer.domElement);
+renderer.setClearColor(0xFFFFFF, 1);
+//document.getElementById("main-container").appendChild(renderer.domElement);
 
-controls = new THREE.TrackballControls( camera );
-
-controls.rotateSpeed = 1.0;
-controls.zoomSpeed = 1.2;
-controls.panSpeed = 0.8;
-
-controls.noZoom = false;
-controls.noPan = false;
-
-controls.staticMoving = true;
-controls.dynamicDampingFactor = 0.3;
+//controls = new THREE.TrackballControls( camera );
+//
+//controls.rotateSpeed = 1.0;
+//controls.zoomSpeed = 1.2;
+//controls.panSpeed = 0.8;
+//
+//controls.noZoom = false;
+//controls.noPan = false;
+//
+//controls.staticMoving = true;
+//controls.dynamicDampingFactor = 0.3;
 
 // Setup scene
-var object_width = 4;
-var row_size = 18;
-var row_width = object_width * row_size;
+var promises = [];
+function appendTile(n) {
+  var modelName = 'roadTile_' + ('000' + n).slice(-3);
+  promises.push(loadModel(modelName).then(function(object) {
+    scene.add(object);
+    render();
+
+    var data = renderer.domElement.toDataURL("image/png");
+    var img = document.createElement("img");
+    img.width = 300;
+    img.src = data;
+    img.dataset.name = modelName + '.png';
+
+    document.getElementById('tiles').appendChild(img);
+
+    scene.remove(object);
+  }).catch(console.error.bind(console)));
+}
 for (var i = 1; i <= 302; i++) {
-  (function(index) {
-    var num = ('000' + index).slice(-3);
-    loadModel('roadTile_' + num).then(function(object) {
-      var horizontal_offset = object_width * (index - 1);
-      object.position.z = -(horizontal_offset % row_width - row_width/2);
-      object.position.x = Math.floor(horizontal_offset / row_width) * object_width - row_width/2;
+  appendTile(i);
+}
 
-      scene.add(object);
+Promise.all(promises).then(console.log("Done!"));
 
-      console.log('Finished loading ' + index);
-    }).catch(console.log.bind(console));
-  })(i);
+document.getElementById('save-all').onclick = function saveAll() {
+  var zip = new JSZip();
+
+  var tiles = document.getElementById('tiles').children;
+  for (var i = 0; i < tiles.length; i++) {
+    var tile = tiles[i];
+    zip.file(tile.dataset.name, tile.src.substr(tile.src.indexOf(',')+1), {base64: true});
+  }
+
+  console.log("Generated zip with " + tiles.length + " items");
+
+  var content = zip.generate({type: "blob"});
+  saveAs(content, "tiles.zip");
 }
 
 function render() {
-	requestAnimationFrame(render);
-
-  controls.update();
+  //controls.update();
 	renderer.render(scene, camera);
 }
-render();
+
+function loop() {
+  requestAnimationFrame(loop);
+  render();
+}
+loop();
