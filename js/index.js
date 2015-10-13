9b32799e6d89b369afdff82309702a6662ef5afe
Sunjay Varma <varma.sunjay@gmail.com>
2015-10-12 23:03:42 -0400

    Made it so that editor is full screen

--- js/index.js
+++ js/index.js
@@ -1,5 +1,27 @@
-var WINDOW_WIDTH = 600;
-var WINDOW_HEIGHT = 400;
+function windowWidth() {
+  return window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+}
+
+function windowHeight() {
+  return window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+}
+
+function onWindowResize(event) {
+  var width = windowWidth();
+  var height = windowHeight();
+
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(width, height);
+}
+window.addEventListener('resize', onWindowResize, false);
+
+var WINDOW_WIDTH = windowWidth();
+var WINDOW_HEIGHT = windowHeight();
+var TILE_SIZE = 3;
+var GRID_LINES = 17;
+var GRID_SIZE = TILE_SIZE * GRID_LINES;
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, WINDOW_WIDTH / WINDOW_HEIGHT, 0.1, 1000);
@@ -15,10 +37,10 @@ renderer.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 renderer.setClearColor(0xEEEEFF, 1);
 document.getElementById("main-container").appendChild(renderer.domElement);
 
-controls = new THREE.TrackballControls( camera );
+controls = new THREE.TrackballControls(camera);
 
-controls.rotateSpeed = 1.0;
-controls.zoomSpeed = 1.2;
+controls.rotateSpeed = 1.4;
+controls.zoomSpeed = 2;
 controls.panSpeed = 0.8;
 
 controls.noZoom = false;
@@ -28,6 +50,14 @@ controls.staticMoving = true;
 controls.dynamicDampingFactor = 0.3;
 
 // Setup scene
+var axisHelper = new THREE.AxisHelper(52);
+axisHelper.position.z = 0.05;
+scene.add(axisHelper);
+
+var gridHelper = new THREE.GridHelper(GRID_SIZE, TILE_SIZE);
+scene.add(gridHelper);
+
+loadModel('roadTile_201').then(scene.add.bind(scene));
 
 function render() {
   controls.update();
