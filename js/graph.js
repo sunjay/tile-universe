8544b77fc0c26002566d7c975f81f7dd24a15897
Sunjay Varma <varma.sunjay@gmail.com>
2015-11-06 10:31:46 -0500

    Made it so that graph vertices are reduced based on distance

--- js/graph.js
+++ js/graph.js
@@ -21,12 +21,77 @@ Graph.prototype.createNode = function(position, material) {
   return node;
 };
 
+Graph.prototype.connect = function(node1, node2) {
+  node1.addAdjacent(node2);
+  node2.addAdjacent(node1);
+};
+
+/**
+ * Merges node 2 into node 1 and removes node 2
+ */
+Graph.prototype.merge = function(node1, node2) {
+  if (node1.material !== node2.material) {
+    throw new Error("Attempt to merge two different materials");
+  }
+  if (node1.id === node2.id) {
+    throw new Error("Attempt to merge the same node");
+  }
+
+  node1.position.add(node2.position).divideScalar(2);
+
+  node2.adjacents.forEach(function(aid) {
+    if (aid !== node1.id) {
+      var adj = this.getNode(aid);
+      if (!adj) {
+        throw new Error("Expected the adjacent with ID = " + aid + " to exist (node ID = " + this.id + ")");
+        return;
+      }
+      this.connect(node1, adj);
+    }
+  }.bind(this));
+
+  this.remove(node2);
+};
+
+Graph.prototype.remove = function(node) {
+  node.adjacents.forEach(function(aid) {
+    var adj = this.getNode(aid);
+    adj.removeAdjacent(node);
+  }.bind(this));
+
+  delete this.nodes[node.id];
+};
+
 /**
  * Merges nearby nodes of the same material
  * Results in a more connected graph with fewer nodes
  */
 Graph.prototype.reduce = function() {
-  //TODO
+  console.log("Original graph size:", Object.keys(this.nodes).length);
+
+  // Any nodes closer than this threshold will be merged
+  var reductionThreshold = 0.1;
+
+  this.nodeIds().forEach(function(nid1) {
+    var node = this.getNode(nid1);
+    if (!node) {
+      return;
+    }
+
+    this.nodeIds().forEach(function(nid2) {
+      if (nid1 === nid2) {
+        return;
+      }
+      var other = this.getNode(nid2);
+      var distance = node.position.distanceTo(other.position);
+
+      if (node.material === other.material && distance < reductionThreshold) {
+        this.merge(node, other);
+      }
+    }.bind(this));
+  }.bind(this));
+
+  console.log("Reduced graph size:", Object.keys(this.nodes).length);
 };
 
 function Node(id, position, material) {
@@ -37,5 +102,22 @@ function Node(id, position, material) {
 }
 
 Node.prototype.addAdjacent = function(node) {
+  if (!node.id) {
+    throw new Error("No node ID");
+  }
+  if (node.id === this.id) {
+    throw new Error("Attempt to add self as adjacent");
+  }
+  if (this.adjacents.indexOf(node.id) >= 0) {
+    return;
+  }
   this.adjacents.push(node.id);
+};
+
+Node.prototype.removeAdjacent = function(node) {
+  var index = this.adjacents.indexOf(node.id);
+  if (index < 0) {
+    throw new Error("Attempt to remove adjacent that is not an adjacent");
+  }
+  return this.adjacents.splice(index, 1)[0];
 };
